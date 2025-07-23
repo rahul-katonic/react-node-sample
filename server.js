302f815f6423dc3850e7f24964b65725a101544a
Aakash Kumar <aakash.kumar@katonic.ai>
2025-07-24 03:22:57 +0530

    chore: subpath

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const SUBPATH = process.env.ROUTE || '/app';
 // Serve static files under the subpath
 app.use(SUBPATH, express.static(path.join(__dirname, 'build')));
 
+app.get('/', (req, res) => {
+  res.status(200).send('OK');
+});
 // For all other routes under subpath, serve index.html (for SPA routing)
 app.get(`${SUBPATH}/*`, (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
