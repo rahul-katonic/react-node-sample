89a0182bb3d1eb2b669edcf2ea7b8e6df3c3b747
Aakash Kumar <113413871+Aakashkk14@users.noreply.github.com>
2025-07-23 15:04:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = 8050;
 
 // Subpath to serve the app on
 const SUBPATH = '/app';
